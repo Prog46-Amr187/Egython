4562494f24dc8ed5dd51868aaac5802f57df699d
RallsTeam <92330230+RallsTeam@users.noreply.github.com>
2021-10-13 02:28:08 +0300

    Update بينترست.py

--- userbot/plugins/بينترست.py
+++ userbot/plugins/بينترست.py
@@ -37,8 +37,8 @@ async def _(event):
     else:
         pass
     A = await event.respond("**╮•⎚ جـارِ التحميل ... 🧸🎈**")
-    ZZZZL1L = get_download_url(M)
-    await event.client.send_file(event.chat.id, ZZZZL1L)
+    N7QQQ = get_download_url(M)
+    await event.client.send_file(event.chat.id, N7QQQ)
     await A.delete()
 
 
