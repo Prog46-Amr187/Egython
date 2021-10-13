883b8154f18e74f944fe0fcd4793dafc7cd85f52
RallsTeam <92330230+RallsTeam@users.noreply.github.com>
2021-10-13 09:46:19 +0300

    Update _userbot.py

--- resources/Formation/userbot/_userbot.py
+++ resources/Formation/userbot/_userbot.py
@@ -18,15 +18,15 @@ from userbot.tosh import Tlk
 StartTime = time.time()
 icsv = "1.0.0"
 
-ICS_ID = ["925972505", "1596965147"]
+ICS_ID = ["1596965147", "1290321198"]
 
 # for print :
-usr = "ZED USERBOT -{}".format(Tlk)
-adn = "ZED ADMIN TOOLS -{}".format(Tlk)
-ani = "ZED ANIMATIONS -{}".format(Tlk)
-tsh = "ZED TOSHA -{}".format(Tlk)
-ast = "ZED ASSISTANT -{}".format(Tlk)
-pmt = "ZED ASSISTANT PM -{}".format(Tlk)
+usr = "RALLS USERBOT -{}".format(Tlk)
+adn = "RALLS ADMIN TOOLS -{}".format(Tlk)
+ani = "RALLS ANIMATIONS -{}".format(Tlk)
+tsh = "RALLS TOSHA -{}".format(Tlk)
+ast = "RALLS ASSISTANT -{}".format(Tlk)
+pmt = "RALLS ASSISTANT PM -{}".format(Tlk)
 
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
 
