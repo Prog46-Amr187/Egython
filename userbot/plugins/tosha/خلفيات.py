7e9f445a3c3ca75547d7530d3fb3279aa74a6932
RallsTeam <92330230+RallsTeam@users.noreply.github.com>
2021-10-13 08:30:45 +0300

    Update خلفيات.py

--- userbot/plugins/tosha/خلفيات.py
+++ userbot/plugins/tosha/خلفيات.py
@@ -1,5 +1,5 @@
 """
-©Zed : @ZedThon
+©Ralls : @RallsThon
   - Commends of All Wallpapers
 """
 
@@ -7,17 +7,17 @@ from . import *
 
 @bot.on(admin_cmd(pattern="م31"))
 @bot.on(sudo_cmd(pattern="م31", allow_sudo=True))
-async def icss(zalzal):
-    await eor(zalzal, W)
+async def icss(Ralls):
+    await eor(Ralls, W)
 
 @bot.on(admin_cmd(pattern="خلفيات1"))
 @bot.on(sudo_cmd(pattern="خلفيات1", allow_sudo=True))
 async def icss(zalzal):
-    await eor(zalzal, WL)
+    await eor(Ralls, WL)
     
 @bot.on(admin_cmd(pattern="خلفيات2"))
 @bot.on(sudo_cmd(pattern="خلفيات2", allow_sudo=True))
 async def icss(zalzal):
-    await eor(zalzal, BN)
+    await eor(Ralls, BN)
 
 
