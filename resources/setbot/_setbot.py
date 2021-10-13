4924c07416bc00ce0f2d21c86d127c285c6ed28b
RallsTeam <92330230+RallsTeam@users.noreply.github.com>
2021-10-13 10:03:47 +0300

    Update _setbot.py

--- resources/setbot/_setbot.py
+++ resources/setbot/_setbot.py
@@ -1,4 +1,4 @@
-#    Copyright (C) zed ~ ANL0KE 2021
+#    Copyright (C) Ralls ~ ANL0KE 2021
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
@@ -62,4 +62,4 @@ async def setbot():
 
 # End For icss bot Only
 # Owner ~ ANL0KE
-# Tele ~ @Zedthon
+# Tele ~ @RallsThon
