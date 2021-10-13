59ef5d3c105cad21709a35a6fe1650c0018a7cb4
RallsTeam <92330230+RallsTeam@users.noreply.github.com>
2021-10-13 04:44:30 +0300

    Update جوجل بلاي.py

--- userbot/plugins/جوجل بلاي.py
+++ userbot/plugins/جوجل بلاي.py
@@ -1,7 +1,7 @@
 """Fetch App Details from Playstore.
 .app <app_name> to fetch app details.
 .appr <app_name>  to fetch app details with Xpl0iter request link.
-  ©ZED™ - @ZlZZl77 """
+  ©Ralls™ - @N7QQQ """
 
 import bs4
 import requests
