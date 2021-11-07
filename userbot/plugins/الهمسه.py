947d780444c0f04084e6640bedbe28ba11945c2a
RallsTeam <92330230+RallsTeam@users.noreply.github.com>
2021-11-07 15:07:44 +0300

    Update الهمسه.py

--- userbot/plugins/الهمسه.py
+++ userbot/plugins/الهمسه.py
@@ -2,7 +2,7 @@ from . import reply_id as rd
 from userbot.tosh import *
 
 
-WPIC = "https://telegra.ph/file/f29ed073927e1f99e02b9.jpg"
+WPIC = "https://telegra.ph/file/b04d993ced4ee4f05b473.jpg"
 T = "𓆰 𝗦𝗢𝗨𝗥𝗖𝗘 𝐑𝐀𝐈𝐈𝐒 - 𝑺𝑬𝑪𝑹𝑬𝑻 𝑴𝑺𝑮 𓆪\n𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n**⌔∮ قائـمه اوامر الهمسه :** \n⪼ `.الهمسه` لعرض كيفيه ارسال الهمسه من بوتك\n⪼ `.همسه` لارسال همسه عن طريق بوت الهمسه  \n𓍹ⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧⵧ𓍻\n𓆩 𝗦𝗢𝗨𝗥𝗖𝗘 𝐑𝐀𝐈𝐈𝐒 - [قناة السورس](t.me/RallsThon) 𓆪"
 
 @bot.on(admin_cmd(pattern="م21"))
